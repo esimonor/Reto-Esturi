2de82bbe34ca652385d8b810552d76eb4e4745ee
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2019-11-28 10:43:16 +0100

    creadas migraciones, tablas Users y Establishments, tambien he modificado el valor del email de la tabla Password_resets

--- database/migrations/2019_11_28_090755_create_establishments_table.php
+++ database/migrations/2019_11_28_090755_create_establishments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateEstablishmentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('establishments', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('lastName');
-            $table->string('email',128)->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('role');
-            $table->rememberToken();
+            $table->string('type');
+            $table->string('localization');
+            $table->unsignedBigInteger('owner');
             $table->timestamps();
+
+            $table->foreign('owner')->references('id')->on('users');
         });
     }
 
@@ -33,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('establishments');
     }
 }
