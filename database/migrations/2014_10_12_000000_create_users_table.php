efe67888587be7b28ffc6ceb30795038ea827ef4
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2019-11-29 08:53:00 +0100

    cambios de string

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('lastName');
-            $table->string('email',128)->unique();
+            $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('role');
