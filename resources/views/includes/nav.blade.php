fdc203053d5e7efb39d66e8271e60ece13528d31
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2019-11-30 02:19:04 +0100

    Añadidos formularios de inicio de sesion y registro a ventanas modales, faltan los estilos pero son 100% funcionales

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -44,54 +44,21 @@
             @guest
             <!-- Inicio de sesion -->
             <label class="text-white">Inicio de sesion</label>
-            <form class="form-inline" method="POST" action="{{ route('login') }}">
-                @csrf
-                <div class="input-group col">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="basic-addon1">Email</span>
-                    </div>
-                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" placeholder="Email" value="{{ old('email') }}" aria-label="Email" aria-describedby="basic-addon1" required autocomplete="email" autofocus>
-                    @error('email')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                    @enderror
-                </div>
-                <div class="input-group col">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="basic-addon1">Contraseña</span>
-                    </div>
-                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Password" aria-label="contra" aria-describedby="basic-addon1" required autocomplete="current-password">
-                    @error('password')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                    @enderror
-                </div>
-                <button class="btn btn-outline-success" type="submit">Iniciar Sesion</button>
-                <div class="form-group col">
-                    <div class="row">
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                            <label class="form-check-label" for="remember">
-                                {{ __('Remember Me') }}
-                            </label>
-                        </div>
-                    </div>
-                    @if (Route::has('password.request'))
-                    <div class="col">
-                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                            {{ __('Forgot Your Password?') }}
-                        </a>
-                    </div>
-                    @endif
-                </div>
-            </form>
+            <!-- Button modal Inicio de sesion-->
+            <button type="button" class="btn btn-outline-success" data-toggle="modal" data-target="#inicioSesionModal">
+                <b style="font-size:1.2em;">Inicio sesion</b></a>
+            </button>
+            <!-- Modal Resgistro-->
+            @include('includes.modales.modalInicioSesion')
             <div class="dropdown-divider"></div>
             <!-- Registrarse -->
             <label class="text-white">Registrarse</label>
-            <a href="/register" id="botonRegistro" class="btn p-2 m-1"><b style="font-size:1.2em;">Registrarse</b></a>
+            <!-- Button modal Resgistro-->
+            <button type="button" id="botonRegistro" class="btn p-2 m-1" data-toggle="modal" data-target="#registroModal">
+                <b style="font-size:1.2em;">Registrarse</b></a>
+            </button>
+            <!-- Modal Resgistro-->
+            @include('includes.modales.modalRegistro')
             @else
             <li class="nav-item dropdown">
                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
