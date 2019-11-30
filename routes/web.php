df1e166d2a5b2994228ee7624260d179fdad6283
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2019-11-30 02:23:05 +0100

    añadidas rutas de auth a web.php, quiza mas tarde lo cambie pero por ahora lo dejo asi

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,30 @@ Route::get('/contacto', function () {
     return view('contacto');
 })->name('contacto');
 
+// ----------------------------------------
+//Auth::routes();
 
-Auth::routes();
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
+// Registration Routes...
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+Route::resetPassword();
+
+// Password Confirmation Routes...
+Route::confirmPassword();
+
+
+// Email Verification Routes...
+
+Route::emailVerification();
+
+// ----------------------------------------
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/profile', function () {
@@ -30,4 +51,4 @@ Route::get('/profile', function () {
 
 Route::get('/modprofile', function () {
     return view('modprofile');
-})->name('modprofile'); 
+})->name('modprofile');
