868f74041ce8fc412fe4a0440565af02e73001e4
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2019-12-02 13:59:22 +0100

    Cambio de color del fondo de los modales

--- resources/views/includes/modales/modalInicioSesion.blade.php
+++ resources/views/includes/modales/modalInicioSesion.blade.php
@@ -1,7 +1,7 @@
 <!-- Modal Inicio de sesion -->
 <div id="inicioSesionModal" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
-        <div class="modal-content">
+        <div class="modal-content bg-dark text-white">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Formulario inicio de sesion</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -11,27 +11,31 @@
             <div class="modal-body">
                 <form method="POST" action="{{ route('login') }}">
                     @csrf
-                    <div class="input-group col">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="basic-addon1">Email</span>
+                    <div class="form-group row">
+                        <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                        <div class="col-md-6">
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         </div>
-                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" placeholder="Email" value="{{ old('email') }}" aria-label="Email" aria-describedby="basic-addon1" required autocomplete="email" autofocus>
-                        @error('email')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
                     </div>
-                    <div class="input-group col">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="basic-addon1">Contraseña</span>
+
+                    <div class="form-group row">
+                        <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                        <div class="col-md-6">
+                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                            @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         </div>
-                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Password" aria-label="contra" aria-describedby="basic-addon1" required autocomplete="current-password">
-                        @error('password')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
                     </div>
                     <div class="form-group col">
                         <div class="form-check">
@@ -43,11 +47,11 @@
                         </div>
                     </div>
                     <div class="form-group col">
-                    @if (Route::has('password.request'))
+                        @if (Route::has('password.request'))
                         <a class="btn btn-link" href="{{ route('password.request') }}">
                             {{ __('Forgot Your Password?') }}
                         </a>
-                    @endif
+                        @endif
                     </div>
                     <div class="input-group col">
                         <button class="btn btn-outline-success" type="submit">Iniciar Sesion</button>
