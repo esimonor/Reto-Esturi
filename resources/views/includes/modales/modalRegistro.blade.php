1b66025fe6acaa33d5baeee44ec9a55c599f0344
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2019-12-02 20:34:01 +0100

    Modificacion gooter de los modales para que se vean bien

--- resources/views/includes/modales/modalRegistro.blade.php
+++ resources/views/includes/modales/modalRegistro.blade.php
@@ -79,16 +79,22 @@
                         <label for="role" class="col-md-4 col-form-label text-md-right">{{ __('Role') }}</label>
 
                         <div class="col-md-6">
-                            <select name="role">
+                            <select name="rol">
+                                <option value="">Seleccione un Rol</option>
                                 <option value="user">Normal</option>
                                 <option value="owner">Dueño de un local</option>
                             </select>
+                            @error('rol')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         </div>
                     </div>
                     <div class="modal-footer">
                         <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="input-group col">
+                                <button type="submit" class="btn btn-outline-success">
                                     {{ __('Register') }}
                                 </button>
                             </div>
