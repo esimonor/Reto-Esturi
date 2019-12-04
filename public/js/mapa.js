bdded493b7a40697739c31e235f78620b55022a9
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2019-12-04 01:57:17 +0100

    He metido en el nav el cambiador de vistad el mapa y he actualizado la funcion que cogia el valor y lo mostraba

--- public/js/mapa.js
+++ public/js/mapa.js
@@ -22,12 +22,11 @@ $(document).ready(function () {
       map.addLayer(layerLabels);
     }
   }
-  document
-    .querySelector('#basemaps')
-    .addEventListener('change', function (e) {
-      var basemap = e.target.value;
-      setBasemap(basemap);
-    });
+  $('.basemaps').click(function () {
+    var basemap = $(this).attr('id');
+    setBasemap(basemap);
+  });
+
 
   L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
