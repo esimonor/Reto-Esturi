a2ec780ed4e83bdf5171a941508cabb5b1825f09
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2019-12-04 02:00:07 +0100

    Elimino rutas de auth

--- routes/web.php
+++ routes/web.php
@@ -22,25 +22,6 @@ Route::get('/contacto', function () {
 // ----------------------------------------
 Auth::routes(['verify' => true]);
 
-// // Authentication Routes...
-// Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-// Route::post('login', 'Auth\LoginController@login');
-// Route::post('logout', 'Auth\LoginController@logout')->name('logout');
-
-// // Registration Routes...
-// Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-// Route::post('register', 'Auth\RegisterController@register');
-
-// // Password Reset Routes...
-// Route::resetPassword();
-
-// // Password Confirmation Routes...
-// Route::confirmPassword();
-
-
-// // Email Verification Routes...
-// Route::emailVerification();
-
 Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
 
 // ----------------------------------------
