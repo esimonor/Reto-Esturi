5cac3e934c73abb80c2d9a341ea12237a1428869
esimonor <esimonor18dw@ikzubirimanteo.com>
2020-01-15 09:58:10 +0100

    Solucionado bug cerrar sesion en vista admin

--- resources/views/lugaractual.blade.php
+++ resources/views/lugaractual.blade.php
@@ -1,8 +1,6 @@
 @include('includes.head')
-@include('includes.mapa')
-<title>@lang('Añadir local')</title>
+<title>@lang('Establecimiento con comentarios')</title>
 
-</style>
 </head>
 <body>
     <div class="container col-12">
@@ -11,9 +9,9 @@
         <!-- Foto de perfil, nombre y correo del usuario -->
         <div class="row d-flex justify-content-center">
             <div class="col-6 row">
-                <img src="{{ URL::asset('images/museum.png') }}" class="m-2 border rounded col-10">
+                <img src="{{ URL::asset('images/museum.png') }}" class="m-2 border rounded col-6">
                 
-            <form class="col-3" method="POST" action="/sitio">
+            <form method="POST" action="/sitio">
                 @csrf
                 <input type="hidden" name="favorito" value="1">
                 <input type="hidden" name="form" value="fav">
@@ -21,21 +19,21 @@
                 <input type="hidden" name="idusuario" value="{{Auth::user()->id}}">
 
                 
-                <button class="btn btn-outline-success m-2 col-4">fav</button>
+                <button class="btn btn-outline-success m-2 col-12">fav</button>
             </form>
             </div>
         </div>
-        <div class="col-3 " >
+        <div class="col-6 " >
             <div class="border">
                 <h4>comentarios de los clientes</h4>
                 <div class="dropdown-divider"></div>
                 <div class="container">
-                <form class="col-3" method="POST" action="/sitio">
+                <form class="col-6" method="POST" action="/sitio">
                 @csrf
                 <input type="text" name="comentario">
                 <input type="hidden" name="form" value="com"> 
                 <input type="hidden" name="id" value="{{$idd}}">               
-                <button class="btn btn-outline-success m-2 col-4">publicar comentario</button>
+                <button class="btn btn-outline-success m-1 col-9">publicar comentario</button>
             </form>
               </div>
             </div>
