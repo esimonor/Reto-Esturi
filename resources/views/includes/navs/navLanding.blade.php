bae763c6abeb5e1a44a57070fc92a0892a35955e
samuelroam <samuelroam@gmail.com>
2020-01-15 10:16:59 +0100

    Cambiar perfil por homeAdmin en navbar

--- resources/views/includes/navs/navLanding.blade.php
+++ resources/views/includes/navs/navLanding.blade.php
@@ -32,10 +32,14 @@
 					</a>
 					<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
 						@if (Auth::user()->role=="owner")
-                        	<a class="dropdown-item bg-dark text-light" href="{{ URL::asset('homeOwner') }}">@lang('Perfil')</a>  
-                    	@else
-                        	<a class="dropdown-item bg-dark text-light" href="{{ URL::asset('profile') }}">@lang('Perfil')</a>
-						@endif
+                            <a class="dropdown-item bg-dark text-light" href="{{ URL::asset('homeOwner') }}">@lang('Perfil')</a>  
+                        @endif
+                        @if (Auth::user()->role=="user")
+                            <a class="dropdown-item bg-dark text-light" href="{{ URL::asset('profile') }}">@lang('Perfil')</a>
+                        @endif
+                        @if (Auth::user()->role=="admin")
+                            <a class="dropdown-item bg-dark text-light" href="{{ URL::asset('homeAdmin') }}">Administrar página</a>
+                        @endif
 						<a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">@lang('Cerrar sesion')</a>
 						<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
 							@csrf
