07fed30ab5498abf4ed499f9d1629563467e4c74
samuelroam <samuelroam@gmail.com>
2020-01-15 12:09:37 +0100

    cambio en la vista del mapa para dispositivos moviles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,28 +6,28 @@
 <body>
     @include('includes.navs.nav')
     <div class="container col-12">
-    <div class="container col-6 float-left">
+        <div class="container  col-xl-6 col-lg-6 col-md-12 col-sm-12 float-left">
     
-        @if (isset($sites))
-            <?php
-                $i=1;
-                foreach ($sites as $site){
-                    ?>
-                @include('includes/tarjetas')
+            @if (isset($sites))
                 <?php
-                $coord=$site->localization;
-                $desc=$site->type;
-                echo '<input id="',$i,'" type="hidden" value="', $coord ,'" />';
-                echo '<input id="name',$i,'" type="hidden" value="',$site->name ,'" />';
-                echo '<input id="desc',$i,'" type="hidden" value="', $desc ,'" />';
-
-                $i++;
-                }
-                echo '<input id="res_final" type="hidden" value="', $i-1 ,'" />';
-            ?>
-        @endif
-    </div>
-    <div class="container col-6 float-left">
+                    $i=1;
+                    foreach ($sites as $site){
+                        ?>
+                    @include('includes/tarjetas')
+                    <?php
+                    $coord=$site->localization;
+                    $desc=$site->type;
+                    echo '<input id="',$i,'" type="hidden" value="', $coord ,'" />';
+                    echo '<input id="name',$i,'" type="hidden" value="',$site->name ,'" />';
+                    echo '<input id="desc',$i,'" type="hidden" value="', $desc ,'" />';
+    
+                    $i++;
+                    }
+                    echo '<input id="res_final" type="hidden" value="', $i-1 ,'" />';
+                ?>
+            @endif
+        </div>
+        <div class="container col-xl-6 col-lg-6 col-md-12 col-sm-12 float-left">
                 <section>
             <!-- Mapa -->
             <div id="map"></div>
