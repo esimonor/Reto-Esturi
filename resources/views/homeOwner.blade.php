a98781586ec8f27d8289a7412c70dff5edb3bed2
samuelroam <samuelroam@gmail.com>
2020-01-27 11:05:47 +0100

    homeOwner responsive para moviles

--- resources/views/homeOwner.blade.php
+++ resources/views/homeOwner.blade.php
@@ -6,25 +6,31 @@
 </head>
 
 <body>
+    <div class="row">
+        <div class="col-12">
+    @include('includes.navs.navUser')
+        </div>
+    </div>
     <!-- Guarda la id del usuario en variable de sesion para usar en controladores hasta encontrar otra mejor forma -->
     <?php $id=Auth::user()->id;session(['id' => $id]); ?>
-    @include('includes.navs.navUser')
     <div class="container col-12">
 
         <!-- Foto de perfil, nombre y correo del usuario -->
         <div class="row">
-            <div class="col-6 row m-2">
+            <div class="alturaForm col-xl-6 col-lg-6 col-md-12 col-sm-12 row m-2">
+                <div class="col-12">
                 <h4 class="titulos col-12">Informacion de usuario</h4>
                 <!--<img src="{{ URL::asset('images/profile.png') }}" class="m-2 border rounded col-10">-->
-                <p class="titulos col-4">Nombre:  {{Auth::user()->name}}</p>
-                <p class="titulos col-4">Apellido: {{Auth::user()->lastName}}</p>
+                <p class="titulos col-6">Nombre:  {{Auth::user()->name}}</p>
+                <p class="titulos col-6">Apellido: {{Auth::user()->lastName}}</p>
                 <p class="titulos col-10">Correo:  {{Auth::user()->email}}</p>
                 <p class="titulos col-10">Tu rol:  {{Auth::user()->role}}</p>
                 <a href="{{route('addlocal')}}" class="btn btn-primary m-2 col-8">Añadir local</a>             
                 <button class="btn btn-danger m-2 col-4">Eliminar sitio</button>
                 <a href="{{ URL::asset('modprofile') }}" class="btn btn-warning m-2 col-4">Modificar cuenta</a>
+                </div>
             </div>
-            <div class="col-6 row">
+            <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 row">
                     @foreach($sites as $site)
                     <div class="col-6">
                         <div class="tarjetas">
@@ -32,15 +38,19 @@
                             <p>Nombre: {{ $site->name }}</p>
                             <p>Tipo de establecimiento: {{ $site->type}}</p>
                             <p>Localización: {{ $site->localization }}</p>
-                            <a class="btn btn-info" value="{{$site->id}}" href="{{route('editlocal',[$site->id])}}">Modificar establecimiento</a>
+                            <a class="btn btn-info" value="{{$site->id}}" href="{{route('editlocal',[$site->id])}}">Modificar local</a>
                         </div>
                     </div>
                 @endforeach
             </div>
         </div>
     </div>
+    <div class="row">
+        <div class="col-12">
     @include('includes.js')
     @include('includes.footer')
+        </div>
+    </div>
 </body>
 
 </html>
